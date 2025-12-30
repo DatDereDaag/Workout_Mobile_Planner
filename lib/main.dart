76cd8c7bb2ba98b03e03f910ebd26ebd5acd62e0
Arvesh Gosine <arveshgosine@gmail.com>
2025-12-30 02:34:25 -0400

    Added Bottom Navbar V1

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:fitness_app/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: MainScreen());
   }
 }
---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,58 @@
+import 'package:fitness_app/screens/calendar/views/calendar_screen.dart';
+import 'package:fitness_app/screens/catalogue/views/catalogue_screen.dart';
+import 'package:fitness_app/screens/home/views/home_screen.dart';
+import 'package:fitness_app/screens/profile/views/profile_screen.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _screenIndex = 0;
+
+  final List<Widget> screens = [
+    HomeScreen(),
+    ProfileScreen(),
+    CatalogueScreen(),
+    CalendarScreen(),
+  ];
+
+  void _onBottomNavbarTapped(int index) {
+    setState(() {
+      _screenIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Fitness App')),
+      body: screens.elementAt(_screenIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: _onBottomNavbarTapped,
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _screenIndex,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedItemColor: Colors.red.shade700,
+        unselectedItemColor: Colors.red.shade100,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.library_books_rounded),
+            label: 'Catalogue',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.calendar_today),
+            label: 'Calendar',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+      ),
+    );
+  }
+}
