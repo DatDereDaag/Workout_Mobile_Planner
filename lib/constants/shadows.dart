e4aadcbe7511ee3dbefd13e9682e45f742a0031b
Arvesh Gosine <arveshgosine@gmail.com>
2026-01-08 22:38:46 -0400

    restyled exercise card

--- lib/constants/shadows.dart
+++ lib/constants/shadows.dart
@@ -6,25 +6,40 @@ class AppShadows {
       color: Color.fromRGBO(0, 0, 0, 0.4),
       blurRadius: 4,
       spreadRadius: 0,
-      offset: Offset(0, 2),
+      offset: Offset(0, -2),
     ),
     BoxShadow(
       color: Color.fromRGBO(0, 0, 0, 0.3),
       blurRadius: 13,
       spreadRadius: -3,
-      offset: Offset(0, 7),
+      offset: Offset(0, -7),
     ),
     BoxShadow(
       color: Color.fromRGBO(0, 0, 0, 0.2),
       blurRadius: 0,
       spreadRadius: 0,
-      offset: Offset(0, -3),
+      offset: Offset(0, 3),
     ),
     BoxShadow(
       color: Color.fromRGBO(0, 0, 0, 1.0),
       blurRadius: 0,
       spreadRadius: 0,
       offset: Offset(0, 0),
+    ),
+  ];
+
+  static const List<BoxShadow> descriptionlabelShadow = [
+    BoxShadow(
+      color: Color.fromRGBO(0, 0, 0, 0.25),
+      blurRadius: 14,
+      spreadRadius: 0,
+      offset: Offset(0, -14),
+    ),
+    BoxShadow(
+      color: Color.fromRGBO(0, 0, 0, 0.22),
+      blurRadius: 5,
+      spreadRadius: 0,
+      offset: Offset(0, -10),
     ),
   ];
 }
--- lib/screens/home/widgets/exercise_card.dart
+++ lib/screens/home/widgets/exercise_card.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:fitness_app/constants/colors.dart';
 import 'package:fitness_app/constants/shadows.dart';
 import 'package:fitness_app/screens/home/utils/exercise_card_clipper.dart';
@@ -29,7 +27,7 @@ class ExerciseCard extends StatelessWidget {
             child: Stack(
               children: [
                 Container(
-                  height: 140,
+                  height: 160,
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: AssetImage('assets/images/bicep-curl.jpg'),
@@ -37,12 +35,47 @@ class ExerciseCard extends StatelessWidget {
                     ),
                   ),
                 ),
-                //Blur Effect
-                BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
-                  child: Container(
-                    height: 140,
-                    color: const Color.fromARGB(255, 42, 7, 7).withOpacity(0.2),
+                Container(
+                  height: 160,
+                  color: const Color.fromARGB(
+                    255,
+                    42,
+                    7,
+                    7,
+                  ).withValues(alpha: 0.4),
+                ),
+                //Gradient Overlay top left to bottom right
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+                      colors: [
+                        Colors.black.withValues(alpha: 1),
+                        AppColors.backgroundColor.withValues(alpha: 0.4),
+                        AppColors.backgroundColor.withValues(alpha: 0.4),
+                        AppColors.primaryColor.withValues(alpha: 0.7),
+                      ],
+                      stops: [0.0, 0.4, 0.6, 1.0],
+                    ),
+                  ),
+                ),
+                //Gradient Overlay top right to bottom left
+                Container(
+                  height: 160,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topRight,
+                      end: Alignment.bottomLeft,
+                      colors: [
+                        Colors.black.withValues(alpha: 1),
+                        Colors.transparent,
+                        Colors.transparent,
+                        AppColors.primaryColor.withValues(alpha: 1),
+                      ],
+                      stops: [0.0, 0.2, 0.7, 1.0],
+                    ),
                   ),
                 ),
               ],
@@ -57,12 +90,12 @@ class ExerciseCard extends StatelessWidget {
               bottom: 0.0,
             ),
             child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
-              height: 30,
+              padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+              height: 34,
               decoration: BoxDecoration(
-                color: AppColors.primaryColor,
-                borderRadius: BorderRadius.circular(4),
                 boxShadow: AppShadows.labelShadow,
+                color: AppColors.primaryColor.withValues(alpha: 0.6),
+                borderRadius: BorderRadius.circular(4),
               ),
               child: Text(
                 exerciseName.toUpperCase(),
@@ -78,25 +111,45 @@ class ExerciseCard extends StatelessWidget {
           // Sets and Reps Widget
           Positioned(
             bottom: 24,
-            right: 24,
+            right: 28,
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              spacing: 4.0,
+              spacing: 8.0,
               children: [
-                //Sets
+                //Weight
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
-                  height: 20,
+                  height: 24,
                   decoration: BoxDecoration(
                     color: AppColors.primaryColor,
                     borderRadius: BorderRadius.circular(4),
-                    boxShadow: AppShadows.labelShadow,
+                    boxShadow: AppShadows.descriptionlabelShadow,
+                  ),
+                  child: Text(
+                    "60 lbs",
+                    style: TextStyle(
+                      fontFamily: 'BlackOps',
+                      fontSize: 14,
+                      letterSpacing: -0.12,
+                      color: AppColors.textColor,
+                    ),
+                  ),
+                ),
+
+                //Sets
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
+                  height: 24,
+                  decoration: BoxDecoration(
+                    color: AppColors.primaryColor,
+                    borderRadius: BorderRadius.circular(4),
+                    boxShadow: AppShadows.descriptionlabelShadow,
                   ),
                   child: Text(
                     "Sets: 4",
                     style: TextStyle(
                       fontFamily: 'BlackOps',
-                      fontSize: 12,
+                      fontSize: 14,
                       letterSpacing: -0.12,
                       color: AppColors.textColor,
                     ),
@@ -106,36 +159,17 @@ class ExerciseCard extends StatelessWidget {
                 //Reps
                 Container(
                   padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
-                  height: 20,
+                  height: 24,
                   decoration: BoxDecoration(
                     color: AppColors.primaryColor,
                     borderRadius: BorderRadius.circular(4),
-                    boxShadow: AppShadows.labelShadow,
+                    boxShadow: AppShadows.descriptionlabelShadow,
                   ),
                   child: Text(
                     "Reps: 12",
                     style: TextStyle(
                       fontFamily: 'BlackOps',
-                      fontSize: 12,
-                      letterSpacing: -0.12,
-                      color: AppColors.textColor,
-                    ),
-                  ),
-                ),
-                //Weight
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 2.0),
-                  height: 20,
-                  decoration: BoxDecoration(
-                    color: AppColors.primaryColor,
-                    borderRadius: BorderRadius.circular(4),
-                    boxShadow: AppShadows.labelShadow,
-                  ),
-                  child: Text(
-                    "Weight: 60 lbs",
-                    style: TextStyle(
-                      fontFamily: 'BlackOps',
-                      fontSize: 12,
+                      fontSize: 14,
                       letterSpacing: -0.12,
                       color: AppColors.textColor,
                     ),
